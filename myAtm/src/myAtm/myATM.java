3e7c0bbba7bb41219221e96eb61f946752d86706
13-ritika <ritikagarg134@gmail.com>
2022-02-03 22:11:44 +0530

    The first commit

---
+++ myAtm/src/myAtm/myATM.java
@@ -0,0 +1,14 @@
+package myAtm;
+
+import java.io.IOException;
+public class myATM{
+
+	public static void main(String[] args) throws IOException {
+		// TODO Auto-generated method stub
+		Options op = new Options();
+		op.getLogin();
+		return;
+
+	}
+
+}
